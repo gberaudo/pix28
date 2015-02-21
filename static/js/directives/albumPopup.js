a1779d5df42f6ebe6bb70bc76c2af7a2e9bd30c5
Viet Hang Nguyen <viethangybvn@gmail.com>
2015-02-21 12:08:22 +0100

    Add page-deactivate and blacken-out option to popup directive

--- static/js/directives/albumPopup.js
+++ static/js/directives/albumPopup.js
@@ -6,7 +6,16 @@ app.directive('albumPopup', [function() {
 		scope: {},
 		template: '<div ng-transclude></div>',
 		link: function(scope, elem, attrs) {
+			var hide;
+			if ('pageDeactivate' in attrs) {
+				var hide = angular.element('<div class = "pageDeactivate"></div>');
+				angular.element(document.body).append(hide);
+				if ('blackenOut' in attrs) hide.addClass('blackenOut');
+			}
 			document.addEventListener('keydown', keyDownHandle, true);
+			elem.on('$destroy', function(evt) {
+				if (hide) hide.remove();
+			});
 			function keyDownHandle(evt) {
 				if (evt.keyCode == 27) {
 					elem.remove();
